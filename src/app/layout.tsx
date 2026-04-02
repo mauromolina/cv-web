33278ed85c67792e9eae104d4cd56c57b8f4a2be
mauromolina15 <mauro.molina@codeait.com>
2026-04-01 21:33:41 -0300

    fix: favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,12 @@ export const metadata: Metadata = {
   keywords:
     "iglesia, cristo viviente, quilmes, quilmes oeste, comunidad cristiana",
   icons: {
-    icon: "/images/cv.png",
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
   },
 };
 
